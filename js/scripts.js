4094141a3d0c98c69471700fde32c5daf8894eda
tgvecchio <89107686+tgvecchio@users.noreply.github.com>
2021-08-18 23:35:57 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ alert ('Processing');
 
 var url = 'https://newsapi.org/v2/top-headlines?' +
           'sources=bbc-news&' +
-          'apiKey=API_KEY';
+          'apiKey=f5f7ae594ad34dadbe4c0557dc6ed08d';
 var req = new Request(url);
 fetch(req)
     .then(function(response) {
