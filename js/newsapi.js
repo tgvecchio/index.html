d12f1526f803f51ba62dd3942b1f3e05e2cc093d
tgvecchio <89107686+tgvecchio@users.noreply.github.com>
2021-08-20 21:27:31 +0200

    Update newsapi.js

--- js/newsapi.js
+++ js/newsapi.js
@@ -1,3 +1,5 @@
-fetch('https://api.allorigins.win/https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f5f7ae594ad34dadbe4c0557dc6ed08d')
-.then(r =>  r.json().then(data => ({status: r.status, body: data})))
-.then(obj => console.log(obj));
+function foo() {
+  fetch('https://api.allorigins.win/https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f5f7ae594ad34dadbe4c0557dc6ed08d')
+  .then(r =>  r.json().then(data => ({status: r.status, body: data})))
+  .then(obj => console.log(obj));
+}
