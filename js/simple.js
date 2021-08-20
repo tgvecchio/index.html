fcd3a9b33b649f9551dfc2792199441c300e9149
tgvecchio <89107686+tgvecchio@users.noreply.github.com>
2021-08-21 01:39:55 +0200

    Update simple.js

--- js/simple.js
+++ js/simple.js
@@ -3,10 +3,12 @@ const myHeading = document.querySelector('h1');
 var url = 'https://newsapi.org/v2/top-headlines?' +
           'sources=bbc-news&' +
           'apiKey=f5f7ae594ad34dadbe4c0557dc6ed08d';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+!var req = new Request(url);
+!fetch(req)
+!    .then(function(response) {
+!        console.log(response.json());
+!    })
 
-myHeading.textContent = 'Simple';
+var urlpy = new Request(83.187.184.226:12345);
+fetch(urlpy)
+myHeading.textContent = 'Ximple';
